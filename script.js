d86b98ed3186ae5db5458c7c39ebde1497c87fec
Gabriel <136026406+callboyryan@users.noreply.github.com>
2024-01-02 08:14:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-let navbar = document.querySelector(".navbar");
-
-document.querySelector("#menu-btn").onclick = () => {
-  navbar.classList.toggle("active");
-};
-
 $(document).ready(function() {
+  $("#menu-btn").click(function() {
+    $(".navbar").toggleClass("active");
+  });
+
   $(".navbar a").click(function() {
     var target = $(this).attr("href");
     var offset = $(target).offset().top;
-    
+
     $("html, body").animate({
       scrollTop: offset
     }, 800);
-    
+
     return false;
   });
 });
